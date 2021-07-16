5fef6407b255eb95d34cf5a42543f87aee803861
Call-ee <callytran123@gmail.com>
2021-07-16 16:37:36 -0400

    remove console.log

--- Sources/index.js
+++ Sources/index.js
@@ -106,14 +106,12 @@ function convertC(event) {
 
 // Get weather API data //
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "1a1c85ee290054195972d7e505026c70";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
-  console.log(response.data);
   let cityName = document.querySelector(".title");
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector(".tNumber");
